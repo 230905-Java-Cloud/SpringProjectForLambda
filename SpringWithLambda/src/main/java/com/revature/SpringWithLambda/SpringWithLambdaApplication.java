10748bc81a050d7ae7a68b0e381bdb30166ab983
JesterCharles <JesterCharles@gmail.com>
2023-10-13 18:53:29 -0400

    uploading the updates with a docker file that can containerize a Spring Boot Cloud Function for AWS Lambda along with the Kubernetes/function.yaml file used to deploy the function to AWS through k8s

--- SpringWithLambda/src/main/java/com/revature/SpringWithLambda/SpringWithLambdaApplication.java
+++ SpringWithLambda/src/main/java/com/revature/SpringWithLambda/SpringWithLambdaApplication.java
@@ -23,21 +23,21 @@ public class SpringWithLambdaApplication {
 	//I'll be commenting functions out to get unique jar files
 	//in the real world, these functions will typically be separate apps
 
-	@Bean //These functions are considered FUNCTIONAL BEANS - they're executed by lambda functions
-	//this is a FUNCTION type method. takes input, returns output. in this case, Strings.
-	public Function<String, String> uppercase(){
-
-		return userInput -> userInput.toUpperCase(); //return the inputted String in uppercase
-
-	}
+//	@Bean //These functions are considered FUNCTIONAL BEANS - they're executed by lambda functions
+//	//this is a FUNCTION type method. takes input, returns output. in this case, Strings.
+//	public Function<String, String> uppercase(){
+//
+//		return userInput -> userInput.toUpperCase(); //return the inputted String in uppercase
+//
+//	}
 
 
 	//get all items
 	@Bean //Remember, we need these functions to be "functional beans" to be noticed by lambda
 	public Supplier<List<String>> getItems(){
-	
+
 		//this is a supplier function, which will return data to the user
-		
+
 		//realistically, this is where we'd defer to the service (which would get data from the DAO etc)
 		//but here, we'll just an ArrayList of Strings to represent items
 
@@ -51,29 +51,29 @@ public class SpringWithLambdaApplication {
 	}
 
 	//insert item
-	@Bean
-	public Consumer<String> insertItem(){
-
-		//since this is a Consumer type function, we won't return anything
-		//but we CAN see this inputted value printed in the logs for our Lambda
-
-		return value -> System.out.println(value);
-
-	}
-
-
-	//get item by id (path param example)
-	@Bean
-	public Function<APIGatewayProxyRequestEvent, String> getItemById() {
-		return input -> {
-			//Extract path parameters from the event
-			String itemId = input.getPathParameters().get("itemId");
-
-			//The rest of the function logic here
-			String response = "Received itemId: " + itemId;
-
-			return response;
-		};
-	}
+//	@Bean
+//	public Consumer<String> insertItem(){
+//
+//		//since this is a Consumer type function, we won't return anything
+//		//but we CAN see this inputted value printed in the logs for our Lambda
+//
+//		return value -> System.out.println(value);
+//
+//	}
+//
+//
+//	//get item by id (path param example)
+//	@Bean
+//	public Function<APIGatewayProxyRequestEvent, String> getItemById() {
+//		return input -> {
+//			//Extract path parameters from the event
+//			String itemId = input.getPathParameters().get("itemId");
+//
+//			//The rest of the function logic here
+//			String response = "Received itemId: " + itemId;
+//
+//			return response;
+//		};
+//	}
 
 }
